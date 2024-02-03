150c954d9b3428c7fc58132698612eb0b28dedbc
Daiana de Paula <daianaadepaula1@gmail.com>
2024-02-03 13:06:21 -0300

    feat: menu item

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Restaurante from "./assets/hashtaurante.jpeg";
 import "./App.css";
 import { Navegacao } from "./Navegacao";
+import { ItemCardapio } from "./ItemCardapio";
+import { pratosPrincipais, sobremesas, bebidas } from "./cardapio";
 
 export function App() {
   return (
     <>
       <img className="capa" src={Restaurante} alt="Imagem de um restaurante" />
       <Navegacao />
+      <div className="menu">
+        {pratosPrincipais.map((item) => (
+          <ItemCardapio
+            nome={item.nome}
+            preco={item.preco}
+            descricao={item.descricao}
+            imagem={item.imagem}
+          />
+        ))}
+      </div>
     </>
   );
 }
